b4e606f1bae3a547791a1da7bcac67a8424c3ca7
johanngorban <ivangorbanwork@gmail.com>
2026-04-15 22:36:57 +0300

    feat: image_metadata_t

---
+++ Application/include/image.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include <assert.h>
+
+#define IMAGE_METADATA_SIZE (32u)
+
+typedef struct __attribute__((packed)) {
+    uint32_t magic;
+    uint32_t size;
+    uint32_t crc;
+    uint8_t version_major;
+    uint8_t version_minor;
+    uint8_t version_patch;
+    uint8_t reserved[IMAGE_METADATA_SIZE - 15]; // For padding;
+} image_metadata_t;
+
+static_assert(sizeof(image_metadata_t) == IMAGE_METADATA_SIZE, "image_metadata_t must be exactly 32 bytes length");
